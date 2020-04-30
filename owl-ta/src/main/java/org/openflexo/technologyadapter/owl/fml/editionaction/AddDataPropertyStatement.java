b9ae906c9e56dc7c83d7b6f81b73a70d442328d1
Sylvain GUERIN <sylvain.guerin@openflexo.org>
2020-04-30 23:33:07 +0200

    MEDIUM / Removed deprecated API for old FML-pretty-print tooling

--- owl-ta/src/main/java/org/openflexo/technologyadapter/owl/fml/editionaction/AddDataPropertyStatement.java
+++ owl-ta/src/main/java/org/openflexo/technologyadapter/owl/fml/editionaction/AddDataPropertyStatement.java
@@ -47,8 +47,6 @@ import org.openflexo.connie.DataBinding;
 import org.openflexo.connie.DataBinding.BindingDefinitionType;
 import org.openflexo.connie.exception.NullReferenceException;
 import org.openflexo.connie.exception.TypeMismatchException;
-import org.openflexo.foundation.fml.FMLRepresentationContext;
-import org.openflexo.foundation.fml.FMLRepresentationContext.FMLRepresentationOutput;
 import org.openflexo.foundation.fml.FlexoProperty;
 import org.openflexo.foundation.fml.annotations.FML;
 import org.openflexo.foundation.fml.editionaction.AssignationAction;
@@ -120,16 +118,6 @@ public interface AddDataPropertyStatement extends AddStatement<DataPropertyState
 
 		public AddDataPropertyStatementImpl() {
 			super();
-		}
-
-		@Override
-		public String getFMLRepresentation(FMLRepresentationContext context) {
-			FMLRepresentationOutput out = new FMLRepresentationOutput(context);
-			out.append(
-					(getSubject() != null ? getSubject().toString() : "null") + "."
-							+ (getDataProperty() != null ? getDataProperty().getName() : "null") + " = " + getValue().toString() + ";",
-					context);
-			return out.toString();
 		}
 
 		@Override
